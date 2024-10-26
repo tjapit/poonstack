cd7931cbade2e824621cc63ed538849794ef7053
Timothy Japit <timothy.japit@gmail.com>
2024-10-26 23:16:42 +0700

    feat(save): save current harpoon list to poonfile

--- lua/poonstack/init.lua
+++ lua/poonstack/init.lua
@@ -9,10 +9,10 @@ local M = {}
 --
 -- main key would be the repos, and then inside each repos would have
 -- their own branches. The branch_names would hold an array of filepaths
--- that was stored in harpoon. 
+-- that was stored in harpoon.
 --
 -- we need somewhere to store that .json file. Idea would be to copy what
--- undotree did (e.g. ~/.local/state/nvim/poonstack), check what they do  
+-- undotree did (e.g. ~/.local/state/nvim/poonstack), check what they do
 -- with :set undodir?.
 
 -- functions we need:
@@ -22,18 +22,116 @@ local M = {}
 -- - vim.fn.readfile() -> read from the json file
 -- - vim.fn.json_decode() -> decode the readfile() results
 
-M.push = function(project_path, branch_name, harpoon_list) 
+local harpoon = require("harpoon")
+
+---Trims the whitespace from the start and end of a string.
+--
+---@param s string
+---@return string
+---@return integer count
+string.trim = function(s)
+	return string.gsub(s, "^%s*(.-)%s*$", "%1")
 end
 
-M.pop = function(project_path, branch_name) 
+M.config = {
+	cwd = vim.fn.getcwd(),
+	poondir = os.getenv("HOME") .. "/.local/state/nvim/poonstack",
+}
+
+M.setup = function(config)
+	M.config = vim.tbl_deep_extend("force", M.config, config or {})
+	M._create_poondir(M.config.poondir)
+	M.config.poonfile, M.config.poonfile_path = M._create_poonfile(M.config.cwd, M.config.poondir)
 end
 
+M._create_poondir = function(poondir)
+	-- 1 if path is directory
+	-- 0 if path not diirectory or not exists
+	-- nil on error
+	if vim.fn.isdirectory(poondir) then
+		return
+	end
+
+	-- returns 1 if created, 0 if already exists
+	-- nil if error during creation
+	if not vim.fn.mkdir(poondir) then
+		return -- failed to create directory
+	end
+end
+
+M._create_poonfile = function(cwd, poondir)
+	local poonfile = M.get_poonfile(cwd)
+	local poonfile_path = M.get_poonfile_path(cwd, poondir)
+
+	if vim.fn.writefile({}, poonfile_path) == -1 then
+		return
+	end
+
+	return poonfile, poonfile_path
+end
+
+---Returns the filepath that stores the harpoon list for the current workking
+---directory.
+---
+---It replaces all the slashes (/) with percents (%) and the ends with the
+---project directory name with a .json extension.
+---
+---@param cwd string current working directory
+---@return string poonfile the poonfile that stores harpoon list
+M.get_poonfile = function(cwd)
+	return cwd:gsub("/", "%%") .. ".json"
+end
+
+---Returns the absolute path to the poonfile.
+---
+---@param cwd string current working directory absolute path
+---@param poondir string harpoon list storage directory absolute path
+---@return string poonfile_path poonfile absolute path
+M.get_poonfile_path = function(cwd, poondir)
+	return poondir .. "/" .. M.get_poonfile(cwd)
+end
+
+---Saves the current branch's harpoon list onto the poonfile
+--
+---@param branch string current branch
+---@param harpoon_list any items on the harpoon list
+M.save = function(branch, harpoon_list)
+	if not vim.fn.filereadable(M.poonfile_path) then
+		return
+	end
+
+	branch = branch:trim()
+	local poonlist_json = vim.json.encode({
+		[branch] = harpoon_list,
+	})
+	vim.fn.writefile({ poonlist_json }, M.config.poonfile_path)
+end
+
+M.push = function(branch)
+	-- code
+end
+
+---Returns the harpoon list for the given branch
+M.pop = function(branch)
+	return { branch }
+end
+
+M.setup()
+M.save(vim.fn.system("git branch --show-current"), harpoon:list().items)
+M.push(vim.fn.system("git branch --show-current"))
 --[[
-lua require("poonstack").push("~/code/lua/plugins/poonstack.nvim/", "main", {
+lua require("poonstack").push("master", {
+  {
+    context = {
+      row = 0,
+      col = 0,
+    },
+    value = "src/app/auth/embed-kyc.component.ts"
+  },
   ...
 })
 
-lua require("poonstack").pop("~/code/lua/plugins/poonstack.nvim/", "main")
+lua require("poonstack").pop("master")
 --]]
 
 return M
--- plugin/example.lua
+++ plugin/example.lua
@@ -1,12 +1,13 @@
 vim.notify("Hello, Tim")
 
 -- testing reading from json and decoding
-local harpoon = require("harpoon")
-local json_file = vim.fn.readfile("example.json")
-local json_example = vim.fn.json_decode(json_file)
+-- local harpoon = require("harpoon")
+-- local json_file = vim.fn.readfile("example.json")
+-- local json_example = vim.fn.json_decode(json_file)
 -- P(json_example["milky-way"])
-harpoon:list():add({
-	context = {},
-	value = "README.md",
-})
-P(harpoon:list().items)
+-- harpoon:list():add({
+-- 	context = {},
+-- 	value = "README.md",
+-- })
+-- P(harpoon:list().items)
+require("poonstack")
